9be192561a9b130e275b69978cb3bcee4a2783b2
deyvissonsouza100 <deyvissonsouza100@gmail.com>
2026-03-02 09:26:51 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   // Ajuste para o nome do repositório no GitHub Pages
-  base: "/gcm-ferias-dashboard/",
+  base: "/Ferias-GCM-/",
 });
